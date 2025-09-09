7f01c570e57c332d70a3c5a71c3e090b8a277c38
zzk13872229834@163.com <zzk13872229834@163.com>
2025-09-09 10:38:44 +0800

    complete class Timestamp

--- include/Timestamp.h
+++ include/Timestamp.h
@@ -6,6 +6,10 @@
 class Timestamp
 {
 public:
-
+    Timestamp();
+    explicit Timestamp(int64_t microSecondsSinceEpoch);
+    static Timestamp now();
+    std::string toString() const;
 private:
+    int64_t microSecondsSinceEpoch_;
 };
