6a0cf8f8bd4180c610389ba92c49550e8772093e
mwm1c <zzk13872229834@163.com>
2025-09-14 15:49:12 +0800

    complete class Acceptor

---
+++ src/Acceptor.cc
@@ -0,0 +1,65 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <errno.h>
+#include <unistd.h>
+
+#include "Acceptor.h"
+#include "Logger.h"
+#include "InetAddress.h"
+
+static int createNonblocking()
+{
+    int sockfd = ::socket(AF_INET, SOCK_STREAM | SOCK_NONBLOCK | SOCK_CLOEXEC, IPPROTO_TCP);
+    if (sockfd < 0)
+    {
+        LOG_FATAL("%s:%s:%d listen socket create err:%d\n", __FILE__, __FUNCTION__, __LINE__, errno);
+    }
+    return sockfd;
+}
+
+Acceptor::Acceptor(EventLoop *loop, const InetAddress &listenAddr, bool reuseport)
+    : loop_(loop), acceptSocket_(createNonblocking()), acceptChannel_(loop, acceptSocket_.fd()), listenning_(false)
+{
+    acceptSocket_.setReuseAddr(true);
+    acceptSocket_.setReusePort(true);
+    acceptSocket_.bindAddress(listenAddr);
+    acceptChannel_.setReadCallback(std::bind(&Acceptor::handleRead, this));
+}
+
+Acceptor::~Acceptor()
+{
+    acceptChannel_.disableAll();
+    acceptChannel_.remove();
+}
+
+void Acceptor::listen()
+{
+    listenning_ = true;
+    acceptSocket_.listen();
+    acceptChannel_.enableReading();
+}
+
+void Acceptor::handleRead()
+{
+    InetAddress peerAddress;
+    int connfd = acceptSocket_.accept(&peerAddress);
+    if (connfd >= 0)
+    {
+        if (NewConnectionCallback_)
+        {
+            NewConnectionCallback_(connfd, peerAddress);
+        }
+        else
+        {
+            ::close(connfd);
+        }
+    }
+    else
+    {
+        LOG_ERROR("%s:%s:%d accept err:%d\n", __FILE__, __FUNCTION__, __LINE__, errno);
+        if (errno == EMFILE)
+        {
+            LOG_ERROR("%s:%s:%d sockfd reached limit\n", __FILE__, __FUNCTION__, __LINE__);
+        }
+    }
+}
