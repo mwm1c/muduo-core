311024af90c8ec3d939d3737e9357c0a27987439
mwm1c <zzk13872229834@163.com>
2025-09-20 18:56:48 +0800

    complete class TcpServer

--- src/TcpServer.cc
+++ src/TcpServer.cc
@@ -33,7 +33,12 @@ TcpServer::TcpServer(EventLoop *loop, const InetAddress &listenAddr,
 
 TcpServer::~TcpServer()
 {
-    
+    for (auto &item : connections_)
+    {
+        TcpConnectionPtr conn(item.second);
+        item.second.reset();
+        conn->getLoop()->runInLoop(std::bind(&TcpConnection::connectDestroyed, conn));
+    }
 }
 
 void TcpServer::setThreadNum(int numThreads)
@@ -52,12 +57,45 @@ void TcpServer::start()
 
 void TcpServer::newConnection(int sockfd, const InetAddress &peerAddr)
 {
+    EventLoop *ioLoop = threadPool_->getNextLoop();
+    char buf[64] = {0};
+    snprintf(buf, sizeof buf, "-%s#%d", ipPort_.c_str(), nextConnId_++);
+    std::string connName = name_ + buf;
+
+    LOG_INFO("TcpServer::newConnection [%s] - new connection [%s] from %s\n",
+             name_.c_str(), connName.c_str(), peerAddr.toIpPort().c_str());
+
+    sockaddr_in local;
+    ::memset(&local, 0, sizeof local);
+    socklen_t addrlen = sizeof local;
+    if (::getsockname(sockfd, (sockaddr *)&local, &addrlen) < 0)
+    {
+        LOG_ERROR("sockets::getLocalAddr");
+    }
+
+    InetAddress localAddr(local);
+    TcpConnectionPtr conn(new TcpConnection(ioLoop, connName, sockfd, localAddr, peerAddr));
+    connections_[connName] = conn;
+    conn->setConnectionCallback(connectionCallback_);
+    conn->setMessageCallback(messageCallback_);
+    conn->setWriteCompleteCallback(writeCompleteCallback_);
+
+    conn->setCloseCallback(std::bind(&TcpServer::removeConnection, this, std::placeholders::_1));
+    ioLoop->runInLoop(std::bind(&TcpConnection::connectEstablished, conn));
+
 }
 
 void TcpServer::removeConnection(const TcpConnectionPtr &conn)
 {
+    loop_->runInLoop(std::bind(&TcpServer::removeConnectionInLoop, this, conn));
 }
 
 void TcpServer::removeConnectionInLoop(const TcpConnectionPtr &conn)
 {
+        LOG_INFO("TcpServer::removeConnectionInLoop [%s] - connection %s\n",
+             name_.c_str(), conn->name().c_str());
+        
+        connections_.erase(conn->name());
+        EventLoop *ioLoop = conn->getLoop();
+        ioLoop->queueInLoop(std::bind(&TcpConnection::connectDestroyed, conn));
 }
